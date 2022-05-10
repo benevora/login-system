d6437138219986d607eab7a2bf41bda727cbf854
Ben Evora <65423996+benevora@users.noreply.github.com>
2022-05-10 13:09:15 -0400

    Home,  and Signup update

--- login/src/pages/Home/index.js
+++ login/src/pages/Home/index.js
@@ -1,4 +1,9 @@
-import React from 'react'
+import React from 'react';
+import { useNavigate } from "react-router-dom";
+import Button from "../../components/Button";
+import useAuth from "../../hooks/useAuth";
+import * as C from './styles';
+
 
 const Home = () => {
   return (
--- login/src/pages/Signup/index.js
+++ login/src/pages/Signup/index.js
@@ -1,9 +1,76 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Input from "../../components/Input";
+import Button from "../../components/Button";
+import { Link, useNavigate } from "react-router-dom";
+import useAuth from '../../hooks/useAuth';
+import * as C from "./styles"
+
 
 const Signup = () => {
+  // states creation
+  const [email, setEmail] = useState("");
+  const [emailConf, setEmailConf] = useState("");
+  const [senha, setSenha] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const { signup } = useAuth();
+
+  const handleSignup = () => {
+    if (!email | !emailConf | !senha) {
+      setError("Preencha todos os campos");
+      return;
+    } else if (email !== emailConf) {
+      setError("Os e-mails nao sao iguais");
+      return;
+    }  
+
+    const res = signup(email, senha);
+
+    if (res) {
+      setError(res);
+      return;
+    }
+
+    alert("Usuario cadastrado com sucesso!");
+    navigate("/");
+  };
+
+
+
   return (
-    <div>Signup</div>
+    <C.Container>
+      <C.Label>SISTEMA DE LOGIN</C.Label>
+      <C.Content>
+        <Input
+          type="email"
+          placeholder="Digite seu E-mail"
+          value={email}
+          onChange={(e) => [setEmail(e.target.value), setError("")]}
+        />
+        <Input
+          type="email"
+          placeholder="Confirme seu E-mail"
+          value={emailConf}
+          onChange={(e) => [setEmailConf(e.target.value), setError("")]}
+        />
+        <Input
+          type="password"
+          placeholder="Digite sua Senha"
+          value={emailConf}
+          onChange={(e) => [setSenha(e.target.value), setError("")]}
+        />
+        <C.LabelError>{error}</C.LabelError>
+        <Button Text="Inscrever-se" onClick={handleSignup} />
+        <C.LabelSignin>
+          Ja tem uma conta?
+          <C.Strong>
+            <Link to="/">&nbsp;Enter</Link>
+          </C.Strong>
+        </C.LabelSignin>
+      </C.Content>
+    </C.Container>
   )
 }
 
-export default Signup
+export default Signup;
